aa99e352f2257e8dcb8a41d06fecb356149c9d66
Gerhard Stein <stein@leat.rub.de>
2017-01-22 13:43:52 +0100

    Further automatisations

--- mk/python/3.5.1/build.sh
+++ mk/python/3.5.1/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 pushd src >/dev/null
 
-rm -rf "Python-${VERSION}"
+if [ ! -d "Python-${VERSION}" ]; then
+
+# Control will enter here if $DIRECTORY doesn't exist.
 tar -xf "Python-${VERSION}.tar.xz" || exit 1
 
 pushd "Python-${VERSION}" >/dev/null
@@ -17,16 +19,26 @@ cat > config.site <<-SITE
 	ac_cv_file__dev_ptc=no
 SITE
 ln -sf "${TOOL_PREFIX}/sysroot/usr/include/"{linux,sys}"/soundcard.h"
+
+
 patch -p1  < "${FILESDIR}/${PACKAGE}-cross-compile.patch" || exit 1
 patch -p1  < "${FILESDIR}/${PACKAGE}-python-misc.patch" || exit 1
 patch -p1  < "${FILESDIR}/${PACKAGE}-android-locale.patch" || exit 1
 patch -Ep1 < "${FILESDIR}/${PACKAGE}-android-libmpdec.patch" || exit 1
 patch -p1  < "${FILESDIR}/${PACKAGE}-android-misc.patch" || exit 1
-patch -p1  < "${FILESDIR}/${PACKAGE}-android-print.patch" || exit 1
+#patch -p1  < "${FILESDIR}/${PACKAGE}-android-print.patch" || exit 1 # Does not work yet.
 patch -p1  < "${FILESDIR}/${PACKAGE}-accept4.patch" || exit 1
 
 
+else
+
+pushd "Python-${VERSION}" >/dev/null
+
+fi
+
 ./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-ipv6 --enable-shared --without-ensurepip || exit 1
+
+
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" || exit 1
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" install || exit 1
 
